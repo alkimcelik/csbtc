0751466f6742ffda5ee4f12cb1bb6dfba6394226
hademirbas <demirbas_anil@hotmail.com>
2023-04-12 01:19:36 +0200

    .

--- Project Script.R
+++ Project Script.R
@@ -4,11 +4,13 @@ library(data.table)
 library(ggplot2)
 library(mondate)
 library(zoo)
+#to resolve the local time issue:
+#Sys.setlocale("LC_TIME", "C")
 #Anil
-#df <- read.csv(file= 'C:/Users/asus/Documents/GitHub/csbtc/Bitcoin Historical Data - Investing.com (1).csv')
+df <- read.csv(file= 'C:/Users/asus/Documents/GitHub/csbtc/Bitcoin Historical Data - Investing.com (1).csv')
 
 #Elcin
-df <- read.csv(file= 'C:/Users/Acer/OneDrive - ADA University/Documents/GitHub/csbtc/Bitcoin Historical Data - Investing.com (1).csv')
+#df <- read.csv(file= 'C:/Users/Acer/OneDrive - ADA University/Documents/GitHub/csbtc/Bitcoin Historical Data - Investing.com (1).csv')
 
 #########Data Manipulation########
 names(df)[names(df) == 'Vol.'] <- "Vol"
@@ -40,5 +42,36 @@ df$Change <- as.numeric(sub("%", "", df$Change, fixed=TRUE))/100
 
 df$quarter <- paste(year(df$Date),quarters(df$Date))
 df_price_quarterly <- df %>% group_by(quarter) %>% summarise(avg = mean(Price))
+growth <- integer(50)
+for (i in nrow(df_price_quarterly)) {
+  if (i==1) { 
+    growth[i]=0
+  }
+  else {
+    growth[i]=((df_price_quarterly[i,2]-df_price_quarterly[i-1,2])/df_price_quarterly[i-1,2])*100
+  }
+}
+growth <- data.frame(growth)
+growth <- t(growth)
+df_price_quarterly = df_price_quarterly[-c(52),]
 
 plot(x = as.yearqtr(df_price_quarterly$quarter, format = '%Y Q%q'), y = df_price_quarterly$avg, type = 'l')
+
+#########Potential Drivers########
+#Anil
+df_drivers <- read.csv(file= 'C:/Users/asus/Documents/GitHub/csbtc/current.csv')
+#Elcin
+#df_drivers <- read.csv('C:/Users/Acer/OneDrive - ADA University/Documents/GitHub/csbtc/current.csv')
+
+df_drivers <- df_drivers %>% select(c('sasdate','UNRATE','CPIAUCSL', 'CPILFESL', 'FEDFUNDS', 
+                      'S.P.500', 'S.P..indust', 'S.P.div.yield',
+                      'S.P.PE.ratio')) 
+df_drivers = df_drivers[-c(1,2),]
+df_drivers$sasdate <- as.Date(df_drivers$sasdate, format = "%m/%d/%Y") 
+df_drivers <- df_drivers %>% filter (df_drivers$sasdate >= "2010-06-01")
+df_drivers <- cbind(df_drivers,df_price_quarterly$quarter)
+
+combined_df <- cbind(df_price_quarterly,df_drivers)
+ggplot(combined_df, aes(x=as.yearqtr(combined_df$quarter, format = '%Y Q%q'))) + 
+  geom_line(aes(y = combined_df$avg), color = "darkred") + 
+  geom_line(aes(y = combined_df$UNRATE), color="steelblue", linetype="twodash") 
--- supplement.R
+++ supplement.R
@@ -1,7 +1,11 @@
 library("readxl")
-df <- read.csv('C:/Users/Acer/OneDrive - ADA University/Documents/GitHub/csbtc/current.csv')
+#Anil
+df <- read.csv(file= 'C:/Users/asus/Documents/GitHub/csbtc/current.csv')
+#Elcin
+#df <- read.csv('C:/Users/Acer/OneDrive - ADA University/Documents/GitHub/csbtc/current.csv')
 df <- df %>% select(c('sasdate','UNRATE','CPIAUCSL', 'CPILFESL', 'FEDFUNDS', 
                 'S.P.500', 'S.P..indust', 'S.P.div.yield',
                 'S.P.PE.ratio')) 
 df = df[-c(1,2),]
 df$sasdate <- as.Date(df$sasdate, format = "%m/%d/%Y") 
+df_filtered <- df %>% filter (sasdate >= "2010-12-01")
